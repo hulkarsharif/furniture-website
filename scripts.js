4e4b2a5c2dfdf7c0e0cdda599f8d7e73e51a97fb
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-11-28 15:24:29 -0500

    Create burger menu bar

--- scripts.js
+++ scripts.js
@@ -25,3 +25,25 @@ const closeIcon = document.querySelector(".close-icon");
 closeIcon.addEventListener("click", () => {
     mobileNav.classList.remove("show-menu");
 });
+const logo = document.createElement("img");
+logo.src = "./images/logo.svg.svg";
+logo.alt = "White logo";
+logo.classList.add("logo");
+mobileNav.appendChild(logo);
+logo.style.display = "block";
+logo.style.marginLeft = "210px";
+logo.style.marginTop = "30px";
+
+// Function to create the menu items
+function createMenu() {
+    const menu = document.createElement("ul");
+
+    menu.innerHTML = `
+        <li><a href="">Products</a></li>
+        <li><a href="">Rooms</a></li>
+        <li><a href="">Inspiration</a></li>
+        <li><a href="">Support</a></li>"`;
+    mobileNav.appendChild(menu);
+}
+
+const menu = createMenu();
